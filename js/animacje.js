3a69c2506d8d86f8d9b104bbbf0bdc5bd1340cf8
EqsGIT <malyprogra@gmail.com>
2019-12-13 21:56:59 +0000

    Mario_Ep6

---
+++ js/animacje.js
@@ -0,0 +1,20 @@
+var Animacje = {
+	aktualizacja: function(dane) {
+		Animacje.zadania.niebo(dane);
+		Animacje.zadania.mario(dane);
+	},
+	
+	zadania: {
+		niebo: function(dane) {
+			dane.obiekty.niebo.x -=1;
+			
+			if(dane.obiekty.niebo.x < -1440) {
+				dane.obiekty.niebo.x = 0;
+			}
+		},
+		
+		mario: function(dane) {
+			dane.obiekty.mario.obecnyStan.animacja(dane);
+		}
+	}
+}
---
+++ js/fizyka.js
@@ -0,0 +1,52 @@
+var Fizyka = {
+	aktualizacja: function(dane){
+		Fizyka.zadania.Grawitacja(dane.obiekty.mario);
+		Fizyka.zadania.WykrywanieKolizji(dane);
+	},
+	
+	zadania: {
+		Grawitacja: function(obiekt) {
+			obiekt.obecnyStan = obiekt.stan.skakanie;
+			obiekt.pedY+=1.2;
+			obiekt.y+=obiekt.pedY;
+		},
+		
+		WykrywanieKolizji: function(dane) {
+			var mario = dane.obiekty.mario;
+			
+			var WykrywanieKolizji = function(obiekt) {
+				if(mario.x < obiekt.x +obiekt.w &&
+					mario.x + mario.w > obiekt.x &&
+					mario.y < obiekt.y + obiekt.h &&
+					mario.h + mario.y > obiekt.y) {
+						Fizyka.zadania.Kolizja(dane, obiekt);
+				}
+			};
+			
+			dane.obiekty.tabelaScian.forEach(function (sciana) {
+				WykrywanieKolizji(sciana);
+			});
+		},
+		
+		Kolizja: function(dane, obiekt) {
+			var mario = dane.obiekty.mario;
+			
+			if(obiekt.typ=="sciana") {
+				if (mario.y + mario.h > obiekt.y && (mario.x+ mario.w) > obiekt.x +10 && mario.x < (obiekt.x + obiekt.w) - 10 && mario.pedY >= 0) {
+					mario.obecnyStan = mario.stan.stanie;
+					mario.y = obiekt.y - mario.h;
+					mario.pedY = 0;
+				}
+				
+				if(mario.x + mario.w > (obiekt.x+16) && mario.x<obiekt.x+obiekt.w-16 && mario.y<obiekt.y+obiekt.h && mario.pedY < 0) {
+					mario.y = obiekt.y + obiekt.h;
+					mario.pedY = 1;
+				} else if(mario.x<obiekt.x && mario.y + mario.h > obiekt.y && mario.y < obiekt.y + obiekt.h) {
+					mario.x = obiekt.x - mario.w;
+				} else if(mario.x>obiekt.x && mario.y + mario.h > obiekt.y && mario.y < obiekt.y + obiekt.h) {
+					mario.x = obiekt.x + obiekt.w;
+				}
+			}
+		}
+	}
+}
