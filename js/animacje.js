efda9c62b23978b3ac3a63847e540ad2833e12fb
EqsGIT <malyprogra@gmail.com>
2019-12-13 21:56:26 +0000

    Mario_Ep5

---
+++ js/animacje.js
@@ -0,0 +1,20 @@
+var Animacje = {
+	aktualizacja: function(dane) {
+		Animacje.zadania.Niebo(dane);
+		Animacje.zadania.Mario(dane);
+	},
+	
+	zadania: {
+		Niebo: function(dane) {
+			dane.obiekty.niebo.x -=1;
+			
+			if(dane.obiekty.niebo.x < -1440) {
+				dane.obiekty.niebo.x = 0;
+			}
+		},
+		
+		Mario: function(dane) {
+			dane.obiekty.mario.obecnyStan.animacja(dane);
+		}
+	}
+}
